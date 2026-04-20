23e2951928a38574207fc8874b810ed1a12fe9e8
Ferrer-System <ortizdionicio993@gmail.com>
2026-04-20 12:36:56 -0600

    Modelo Contacto estructurado

--- src/org/generation/hackathon/models/Contacto.java
+++ src/org/generation/hackathon/models/Contacto.java
@@ -1,4 +1,68 @@
 package org.generation.hackathon.models;
 
 public class Contacto {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private int telefono;
+    private static int idContacto = 1;
+
+    public Contacto() {
+    }
+
+    public Contacto(String nombre, String apellido, int telefono) {
+        this.id = idContacto++;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public static int getIdContacto() {
+        return idContacto;
+    }
+
+    public static void setIdContacto(int idContacto) {
+        Contacto.idContacto = idContacto;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", telefono=" + telefono + "]";
+    }
+    
+    
+
+
 }
