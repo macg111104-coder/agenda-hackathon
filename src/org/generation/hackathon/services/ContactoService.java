65d0638c695327039f326e86bf932d72e49ce9b3
Miguel Castro <macg111104@gmail.com>
2026-04-20 15:33:02 -0600

    Agregando la funcion "listarContactos" al archivo ContactoSevice.java

--- src/org/generation/hackathon/services/ContactoService.java
+++ src/org/generation/hackathon/services/ContactoService.java
@@ -1,4 +1,28 @@
 package org.generation.hackathon.services;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class ContactoService {
+
+    public void listarContactos(List<Contacto> agenda) {
+        if (agenda == null || agenda.isEmpty()) {
+            System.out.println("La agenda está vacia... ");
+            return;
+        }
+
+        // Creando una copia para no alterar la lista original si no se especifica
+        List<Contacto> listaOrdenada = new ArrayList<>(agenda);
+
+        // Ordenando alfabéticamente por Nombre y luego por Apellido.
+        listaOrdenada.sort(Comparator.comparing(Contacto::getNombre, String.CASE_INSENSITIVE_ORDER)
+                .thenComparing(Contacto::getApellido, String.CASE_INSENSITIVE_ORDER));
+
+        // Mostrando los contactos con el formato de nombre + apellido + telefono
+        System.out.println("\n--- CONTACTOS ---");
+        for (Contacto c : listaOrdenada) {
+            System.out.println(c.getNombre() + " " + c.getApellido() + " - " + c.getTelefono());
+        }
+    }
 }
